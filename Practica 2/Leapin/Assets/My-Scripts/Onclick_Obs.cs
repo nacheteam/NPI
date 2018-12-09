7b1c948d07f8bfc15f7d37ca081a62c0701b932a
Ignacio Aguilera Martos <nacheteam1996@gmail.com>
2018-12-09 23:40:50 +0100

    Leapin

---
+++ Practica 2/Leapin/Assets/My-Scripts/Onclick_Obs.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Leap;
+using Leap.Unity;
+using Leap.Unity.RuntimeGizmos;
+using Leap.Unity.Attributes;
+using Leap.Unity.Interaction.Internal;
+using Leap.Unity.Query;
+using System;
+using UnityEngine.Events;
+using UnityEngine.Serialization;
+
+public class Onclick_Obs : MonoBehaviour
+{
+    // Use this for initialization
+
+    void OnClick()
+    {
+        Debug.Log("HEI");
+        //SceneManager.LoadScene("ObservationMode");
+    }
+}
---
+++ Practica 2/Leapin/Assets/My-Scripts/Onclick_Play.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Onclick_Play : MonoBehaviour {
+    void OnClick()
+    {
+        Debug.Log("HEI");
+        SceneManager.LoadScene("PlayMode");
+    }
+}
