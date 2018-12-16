b67fcb991d9f4bdf0532f82ddd11fafcdc59702b
Ignacio Aguilera Martos <nacheteam1996@gmail.com>
2018-12-16 12:00:30 +0100

    Ultima versión

--- Practica 2/Leapin/Assets/My-Scripts/Rotation.cs
+++ Practica 2/Leapin/Assets/My-Scripts/Rotation.cs
@@ -31,27 +31,29 @@ public class Rotation : MonoBehaviour {
 
         if (frame.Hands.Count > 0)
         {
+            //Inicializamos manos
             List<Hand> hands = frame.Hands;
             Hand hand0 = hands[0];
-            Vector position = hand0.PalmPosition;
-            Vector velocity = hand0.PalmVelocity;
-            Vector direction = hand0.Direction;
+
+            //Inicializamos Dedos
             bool move = false;
             float velocity_x = hand0.PalmVelocity.ToVector3().x;
             var thumb0 = hand0.Fingers[0];
             var index0 = hand0.Fingers[1];
-            var middle0 = hand0.Fingers[3];
-            var ring0 = hand0.Fingers[4];
+            var middle0 = hand0.Fingers[2];
+            var ring0 = hand0.Fingers[3];
             var pinky0 = hand0.Fingers[4];
+
+            //Si el Leap detecta una mano
             if (frame.Hands.Count == 1)
             {
-                Debug.Log("Direccion");
-                Debug.Log(pinky0.Direction.ToVector3());
+                // Y esa mano es la derecha, entonces
                 if (hand0.IsRight)
                 {
+                    //Miramos la orientación de la palma y en consecuencia lo rotamos en un eje o un otro
                     if (hand0.PalmNormal.ToVector3().x < 0.6 && hand0.PalmNormal.ToVector3().x > -0.3 && hand0.PalmNormal.ToVector3().z > 0)
                     {
-                        david.transform.rotation = Quaternion.Euler(90 * hand0.PalmNormal.ToVector3().y, 0, 0);
+                        david.transform.rotation = Quaternion.Euler(90 * hand0.PalmNormal.ToVector3().y, david.transform.rotation.eulerAngles.y, david.transform.rotation.eulerAngles.z);
                         move = true;
                     }
                     if (hand0.PalmNormal.ToVector3().z > 0 && hand0.PalmNormal.ToVector3().y > -0.3 && hand0.PalmNormal.ToVector3().y < 0.3)
@@ -59,30 +61,29 @@ public class Rotation : MonoBehaviour {
                         david.transform.Rotate(0, 5 * hand0.PalmNormal.ToVector3().x, 0);
                         move = true;
                     }
-                }
-                // x entre 0.3 y 1, y < 0.7, z entre -0.3 y 0.3
-                if (hand0.PalmNormal.ToVector3().x > 0.3 && hand0.PalmNormal.ToVector3().y < -0.7 && hand0.PalmNormal.ToVector3().z < 0.3 && hand0.PalmNormal.ToVector3().z > -0.5)
-                {
-                    if (!index0.IsExtended && !middle0.IsExtended && !ring0.IsExtended && !pinky0.IsExtended && extended_fingers)
+                    // Miramos la orientación de la palma
+                    if (hand0.PalmNormal.ToVector3().x > 0.3 && hand0.PalmNormal.ToVector3().y < -0.7 && hand0.PalmNormal.ToVector3().z < 0.3 && hand0.PalmNormal.ToVector3().z > -0.5)
                     {
-                        Debug.Log("Dedos yano-extendidos");
-                        david.SetActive(true);
+                        // Si los dedos no están extendidos y antes sí, es decir hemos cerrado la mano
+                        if (!index0.IsExtended && !middle0.IsExtended && !ring0.IsExtended && !pinky0.IsExtended && extended_fingers)
+                        {
+                            david.SetActive(true);
+                        }
+                        // Si los dedos están extendidos y antes no, es decir hemos abierto la mano
+                        else if (index0.IsExtended && middle0.IsExtended && ring0.IsExtended && pinky0.IsExtended && !extended_fingers)
+                        {
+                            david.SetActive(false);
+                        }
                     }
-                    else if (index0.IsExtended && middle0.IsExtended && ring0.IsExtended && pinky0.IsExtended && !extended_fingers)
+                    // Miramos si los dedos están extendidos o no
+                    if (index0.IsExtended && middle0.IsExtended && ring0.IsExtended && pinky0.IsExtended)
                     {
-                        Debug.Log("Dedos yasi-extendidos");
-                        david.SetActive(false);
+                        extended_fingers = true;
                     }
-                }
-                if (index0.IsExtended && middle0.IsExtended && ring0.IsExtended && pinky0.IsExtended)
-                {
-                    Debug.Log("Dedos extendidos");
-                    extended_fingers = true;
-                }
-                if (!index0.IsExtended && !middle0.IsExtended && !ring0.IsExtended && !pinky0.IsExtended)
-                {
-                    Debug.Log("Dedos no extendidos");
-                    extended_fingers = false;
+                    if (!index0.IsExtended && !middle0.IsExtended && !ring0.IsExtended && !pinky0.IsExtended)
+                    {
+                        extended_fingers = false;
+                    }
                 }
             }
             /*************************************************************************************************
@@ -90,45 +91,48 @@ public class Rotation : MonoBehaviour {
              * ***********************************************************************************************/
             else if (frame.Hands.Count > 1)
             {
+                // Inicializamos mano
                 Hand hand1 = hands[1];
 
+                //Inicializamos dedos
                 var thumb1 = hand1.Fingers[0];
                 var index1 = hand1.Fingers[1];
-                var middle1 = hand1.Fingers[3];
-                var ring1 = hand1.Fingers[4];
+                var middle1 = hand1.Fingers[2];
+                var ring1 = hand1.Fingers[3];
                 var pinky1 = hand1.Fingers[4];
 
-                Debug.Log("2");
-                Debug.Log(!index0.IsExtended);
-                Debug.Log(!middle0.IsExtended);
-                Debug.Log(!ring0.IsExtended);
-                Debug.Log(pinky0.IsExtended);
-                if (!index0.IsExtended && !middle0.IsExtended && pinky0.IsExtended && !move)
+                // Si los dedos no están extendidos excepto los meñiques de ambas manos
+                if (!index0.IsExtended && !middle0.IsExtended && !ring0.IsExtended && pinky0.IsExtended && !move)
                 {
-                    if (!index1.IsExtended && !middle1.IsExtended && pinky1.IsExtended && !move)
+                    if (!index1.IsExtended && !middle1.IsExtended && !ring0.IsExtended && pinky1.IsExtended && !move)
                     {
-                        Debug.Log("Atras");
+                        // Y los meñiques están juntos
                         var pinkys_distance = Vector3.Distance(pinky0.TipPosition.ToVector3(), pinky1.TipPosition.ToVector3());
-                        Debug.Log(pinkys_distance);
                         if (pinkys_distance < 10){
+                            // Entonces nos vamos al menú de nuevo
                             SceneManager.LoadScene("Menu");
                         }
                     }
                 }
+                // Si los dedos no están extendidos excepto los índices de ambas manos
                 float object_vel = 0;
                 if (index0.IsExtended && !middle0.IsExtended && !ring0.IsExtended && !pinky0.IsExtended && !move)
                 {
                     if (index1.IsExtended && !middle1.IsExtended && !ring1.IsExtended && !pinky1.IsExtended && !move)
                     {
+                        //Miramos la si se están alejando o acercando ambos índices entre sí
                         var distancia_entre_dedos_new = Vector3.Distance(index0.TipPosition.ToVector3(), index1.TipPosition.ToVector3());
+                        // Calculamos una velocidad relativa a mabas manos
                         if (distancia_entre_dedos_old > distancia_entre_dedos_new)
                         {
                             object_vel = hand0.PalmVelocity.ToVector3().x * hand1.PalmVelocity.ToVector3().y / 5000;
+                            // Si se acercan los índices entre sí, ponemos que el david debe dirigirse a su posición alejada (para hacer zoom out)
                             current = 1;
                         }
                         else
                         {
                             object_vel = hand0.PalmVelocity.ToVector3().x * hand1.PalmVelocity.ToVector3().y / 5000;
+                            // Si se alejan los índices entre sí, ponemos que el david debe dirigirse a la posición de la cámara (para hacer zoom in)
                             current = 0;
                         }
                         distancia_entre_dedos_old = distancia_entre_dedos_new;
@@ -136,6 +140,7 @@ public class Rotation : MonoBehaviour {
                 }
                 if (object_vel < 0)
                     object_vel *= -1;
+                // Hacemos que se acerque o se aleje de la cámara según la velocidad de las manos al alejarse o acercarse
                 david.transform.position = Vector3.MoveTowards(david.transform.position, waypoints[current].transform.position, Time.deltaTime * object_vel);
             }
             
